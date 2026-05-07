93964c5456f72345786913a0c8793f7495f8bddb
HVPasindu <hvpmjayarathna2001@gmail.com>
2026-05-07 11:12:00 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
+const express = require("express");
+const cors = require("cors");
+
+//const authRoutes = require("./routes/auth-routes");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//app.use("/api/auth", authRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Job Skill Platform API running...");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
